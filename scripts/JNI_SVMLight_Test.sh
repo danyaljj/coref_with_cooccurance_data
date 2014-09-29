f15f0ec309407dea481805cc66f78776e2edc8bb
penghaoruo <penghaoruo@hotmail.com>
2014-09-29 15:09:50 -0500

    update SVM Light script

--- scripts/JNI_SVMLight_Test.sh
+++ scripts/JNI_SVMLight_Test.sh
@@ -1,1 +1,1 @@
-java -Xmx128m -classpath bin/:lib/* -Djava.library.path=./data/svmlight edu.illinois.cs.cogcomp.cooccurancedata.JNI_SVMLight_Test
+java -Xmx1g -classpath bin/:lib/* -Djava.library.path=./data/svmlight edu.illinois.cs.cogcomp.cooccurancedata.JNI_SVMLight_Test
